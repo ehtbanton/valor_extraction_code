59aa0c08b3171163d063cc5f59eeaf81121f1afc
ehtbanton <notnatheyam@gmail.com>
2025-08-13 13:46:13 +0100

    wip 2

--- src/text_processing.py
+++ src/text_processing.py
@@ -9,26 +9,27 @@ def get_pdd_targets(contents_list):
     pdd_targets = []
     section_heading = ""
     for line in contents_list.splitlines():
-        if "." not in line.split()[0]:
-            section_heading = " ".join(line.split()[1:-1])
-        else:
-            subheading = " ".join(line.split()[1:-1])
-            subheading_idx = line.split()[0]
-            page_num = line.split()[-1]
-            # section heading, subheading title, subheading idx, page num
-            pdd_targets.append((section_heading, subheading, subheading_idx, page_num))
+        if line.strip() and not line.startswith("Contents"):
+            print(line)
+            if "." not in line.split()[0]:
+                section_heading = " ".join(line.split()[1:-1])
+            else:
+                subheading = " ".join(line.split()[1:-1])
+                subheading_idx = line.split()[0]
+                page_num = line.split()[-1]
+                # section heading, subheading title, subheading idx, page num
+                pdd_targets.append((section_heading, subheading, subheading_idx, page_num))
     return pdd_targets
 
 
-#def assemble_user_prompt()
+def assemble_user_prompt(target, infilling_info):
+    # User prompt contains the exact information source from the TEMPLATE.
+    pass
 
 
-
-
-#def assemble_system_prompt() 
-
-
-
+def assemble_system_prompt(infilling_info): # only 
+    # System prompt contains a static description of how to produce output. An EXACT FORMAT.
+    pass
 
 #def is_valid_response(response, infilling_info):
 
