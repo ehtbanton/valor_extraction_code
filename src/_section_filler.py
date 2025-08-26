b10ba4d0fba3941b79447ddfa35be59da94d094e
ehtbanton <notnatheyam@gmail.com>
2025-08-26 13:42:44 +0100

    recent changes

--- src/_section_filler.py
+++ src/_section_filler.py
@@ -3,6 +3,9 @@ from gemini_interface import ask_gemini
 from text_processing import assemble_user_prompt
 
 
+# HOPE TO ALSO HAVE PRELIMINARY INFILLING LOGIC HERE RATHER THAN IN MAIN
+
+
 def refill_section(GEMINI_CLIENT, infilling_info, uploaded_files_cache):
     # Returns a pseudo-response...
     system_prompt = "Search through all user-provided files. In the user prompt, wherever you see 'INFO_NOT_FOUND: <info type>', replace it with the relevant information. Then respond with text identical to the user prompt but with as many replacements made as possible, if you are able to find that info in the provided context. Only respond with this edited user prompt and include no other text in your response."
